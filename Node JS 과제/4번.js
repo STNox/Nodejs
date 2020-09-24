f3021c7c4fd5a6a1c9dd104192f92f24e306e402
STNox <caush@naver.com>
2020-09-24 21:48:22 +0900

    Node JS

--- Node JS 과제/4번.js
+++ Node JS 과제/4번.js
@@ -1,17 +1,17 @@
 let numbers = {0: 0, 1: 0,2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
 for (let i=1; i<=20; i++){
-    let getNum = String(i);
-    for (k=0; k<10; k++) {
-        if (getNum[-1] === k) {
+    let getNum =i;
+    for (let k=0; k<10; k++) {
+        if (getNum % 10 === k) {
             numbers[k] += 1;
         }
-        if (getNum[-2] === k) {
+        if (parseInt(getNum / 10) === k) {
             numbers[k] += 1;
         }
-        if (getNum[-3] === k) {
+        if (parseInt(getNum / 100) === k) {
             numbers[k] += 1;
         }
-        if (getNum[-4] === k) {
+        if (parseInt(getNum / 1000) === k) {
             numbers[k] += 1;
         }
     }
